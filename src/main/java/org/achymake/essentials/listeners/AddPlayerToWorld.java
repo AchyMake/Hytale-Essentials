5d0f7541e2f31d3d4afcd148d95e22e1d372b4cb
AchyMake <achymake@gmail.com>
2026-02-05 15:26:13 +0100

    patched AddPlayerToWorld thus adding player to pvp if world is pvp

--- src/main/java/org/achymake/essentials/listeners/AddPlayerToWorld.java
+++ src/main/java/org/achymake/essentials/listeners/AddPlayerToWorld.java
@@ -1,9 +1,21 @@
 package org.achymake.essentials.listeners;
 
 import com.hypixel.hytale.server.core.event.events.player.AddPlayerToWorldEvent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import org.achymake.essentials.Essentials;
+import org.achymake.essentials.handlers.PlayerHandler;
 
 public class AddPlayerToWorld {
+    private static Essentials getInstance() {
+        return Essentials.getInstance();
+    }
+    private static PlayerHandler getPlayerHandler() {
+        return getInstance().getPlayerHandler();
+    }
     public static void onAddPlayerToWorld(AddPlayerToWorldEvent event) {
         event.setBroadcastJoinMessage(false);
+        var isPVP = event.getWorld().getWorldConfig().isPvpEnabled();
+        var playerRef = event.getHolder().getComponent(PlayerRef.getComponentType());
+        getPlayerHandler().setPVP(playerRef, isPVP);
     }
 }
